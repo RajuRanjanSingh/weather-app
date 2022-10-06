63467c68003a2f79a2c016a94b65cd01b4cdab0a
RajuRanjanSingh <rrsingh02012001@gmail.com>
2022-10-06 18:08:06 +0530

    commit-3

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
+import data from '../Details.json'
 
 const Weather = () => {
   return (
     <>
-        <div className="container-1">
+       {
+        Object.values(data).map((item,index)=>(
+            <div key={index}>
+<div className="container-1">
             <div className='header'>
-                <div className="h2-text">New York City, NY,United States <span className="small-text">As of 04:37 EDT</span></div>
+                <div className="h2-text">{item.results.channel.location.city}, {item.results.channel.location.region} {item.results.channel.location.country} <span className="small-text">As of 04:37 EDT</span></div>
                 <div className="search-bar">
                     <input type="text" className="search-input" />
                 </div>
             </div>
-            <h1 className="h1-text">21&deg;</h1>
+            <h1 className="h1-text">{item.results.channel.item.condition.code}&deg;</h1>
             <h4 className="h4-text">Clear</h4>
-            <span className="text-2">Day 30&deg;</span>
-            <span className="dot"></span>
-            <span className="text-2">Night 20&deg;</span>
+            <span className="text-2">{item.results.channel.item.title}</span>
+           
           </div>
           <div className="container-2">
-            <h3 className="h3-text">Today's Forecast for New York City, NY, United States</h3>
+            <h3 className="h3-text">{item.results.channel.description}</h3>
             <div className="list">
               <div className="list-items">
                 <div className="text">Morning</div>
@@ -36,7 +39,9 @@ const Weather = () => {
               <div className="list-items">
               <div className="text">Morning</div>
                 <div className="deg">25&deg;</div>
-                <div className="icon"></div>
+                <div className="icon">
+                  <img src={item.results.channel.image.url} alt="" />
+                </div>
                 <div className="text">0%</div>
               </div>
               <div className="list-items">
@@ -47,6 +52,11 @@ const Weather = () => {
               </div>
             </div>
           </div>
+            </div>
+            
+        ))
+       }
+        
     </>
   )
 }
